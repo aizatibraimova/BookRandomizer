0f7df1841284c546e5736563e65595cfd03ba405
Aizat Ibraimova <97326728+aizatibraimova@users.noreply.github.com>
2024-05-23 16:38:19 +0000

    Random Book fetcher

--- app.rb
+++ app.rb
@@ -1,9 +1,31 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:index)
+end
+
+
+post("/random_book") do
+  @book_details = fetch_random_book
+  erb(:book_result)
+end
+
+def fetch_random_book
+  base_url = "https://openlibrary.org/search.json"
+  random_page = rand(1..10)
+  response = HTTP.get("#{base_url}?q=the&page=#{random_page}")
+  books = JSON.parse(response.body)["docs"]
+
+  random_book = books.sample
+
+  {
+    title: random_book["title"],
+    author: random_book["author_name"]&.first,
+    pages: random_book["number_of_pages_median"],
+    cover_id: random_book["cover_i"],
+    summary: random_book["first_sentence"]&.first
+  }
 end
